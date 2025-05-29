51cef377988fb6d44100a42eb8727c692efaaadd
Ehsaan Hoque <ehsaanhoque57@gmail.com>
2025-05-29 23:54:51 +0100

    add score, coin sound and music

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,5 +1,8 @@
 extends Area2D
 
-func _on_body_entered(body: Node2D) -> void:
-	print("+1 coin!")
-	queue_free()
+@onready var game_manager: Node = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _on_body_entered(body: Node2D):
+	game_manager.add_point()
+	animation_player.play("pickup")
